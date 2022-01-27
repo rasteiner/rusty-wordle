b2dc6945fec2d18221493c72f31f610c79738c02
Roman Steiner <roman@toastlab.ch>
2022-01-27 16:40:18 +0000

    words can actually contain non unique chars

--- build.rs
+++ build.rs
@@ -12,26 +12,18 @@ fn main() {
 
     //split data into lines
     let lines = data.split("\n");
-    let words = lines.map(|line| line.split("\t").next().unwrap());
-
-    //only keep the 5 letter words
-    let mut words = words.filter(|word| word.len() == 5).map(|word| word.to_string()).collect::<Vec<_>>();
+    let words:Vec<String> = lines
+        .map(|line| line.split("\t").next().unwrap())
+        .filter(|word| word.len() == 5)
+        .map(|word| word.to_string())
+        .collect();
     
-    //retain words that have all unique letters
-    words.retain(|word| {
-        let chars = word.chars();
-        for (i, c) in chars.clone().enumerate() {
-            if chars.clone().skip(i+1).any(|c2| c == c2) {
-                return false;
-            }
-        }
-        true
-    });
-
     //write the words to the file "words-build.txt"
     let mut file = io::BufWriter::new(File::create("src/words.txt").unwrap());
     for word in words.clone() {
         file.write_all(word.as_bytes()).unwrap();
         file.write_all(b"\n").unwrap();
     }
+
+    println!("cargo:rerun-if-changed=data.txt");
 }
