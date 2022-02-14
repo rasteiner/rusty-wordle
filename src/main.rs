07712e2a3b4947146bc05fae0ab06ce6e6121ae7
Roman Steiner <roman@toastlab.ch>
2022-02-14 10:19:16 +0000

    counting rules still not correct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 static DATA: &'static str = include_str!("words.txt");
 
-use std::{io, collections::HashMap};
+use std::{io, collections::HashMap, ops::AddAssign};
+
+#[derive(Copy, Clone, Default)]
+struct CharCount {
+    capped: bool,
+    count: u8,
+}
+
+//AddAssign
+impl AddAssign<u8> for CharCount {
+    fn add_assign(&mut self, rhs: u8) {
+        self.count += rhs;
+    }
+}
+
+#[derive(Copy, Clone)]
+struct CharRule {
+    sure: Option<char>,
+    not: [bool; 26],
+}
 
 fn strong(msg: &str) {
     let len = msg.len();
@@ -16,12 +35,17 @@ fn count_chars(word: &str) -> HashMap<char, u8> {
     counts
 }
 
-fn counts_match(word: &HashMap<char, u8>, pattern: &HashMap<char, u8>) -> bool {
+fn counts_match(word: &HashMap<char, u8>, pattern: &HashMap<char, CharCount>) -> bool {
     for (c, count) in pattern.iter() {
-        if word.contains_key(c) && word[c] != *count {
-            return false;
+        if word.contains_key(c) && word[c] != count.count {
+            if count.capped && word[c] > count.count {
+                return false;
+            }
+            if !count.capped && word[c] < count.count {
+                return false;
+            }
         }
-        if !word.contains_key(c) && *count > 0 {
+        if !word.contains_key(c) && count.count > 0 {
             return false;
         }
     }
@@ -40,11 +64,7 @@ fn get_input() -> Result<[char; 5], u8> {
     Ok([input[0], input[1], input[2], input[3], input[4]])
 }
 
-#[derive(Copy, Clone)]
-struct CharRule {
-    sure: Option<char>,
-    not: [bool; 26],
-}
+
 
 fn matches_charrule(c: char, rule: &CharRule) -> bool {
     if rule.sure.is_some() && rule.sure.unwrap() != c {
@@ -73,7 +93,7 @@ fn main() {
     let lines = DATA.trim().split("\n");
     let mut words = lines.map(|line| line.split("\t").next().unwrap()).collect::<Vec<&str>>();
     
-    let mut counts:HashMap<char, u8> = HashMap::new();
+    let mut counts:HashMap<char, CharCount> = HashMap::new();
 
     let mut char_rules:[CharRule; 5] = [CharRule { sure: None, not: [false; 26] }; 5];
 
@@ -91,15 +111,18 @@ fn main() {
             },
         };
 
-        let mut guesscounts = HashMap::<char, u8>::new();
+        let mut guesscounts = HashMap::<char, CharCount>::new();
 
         for (i, c) in word.chars().enumerate() {
             let res = input[i];
             if res == 'y' || res == 'e' {
-                *guesscounts.entry(c).or_insert(0) += 1;
-            } else if res == 'n' && !guesscounts.contains_key(&c) {
-                //set counts to 0
-                *guesscounts.entry(c).or_insert(0) = 0;
+                *guesscounts.entry(c).or_insert(Default::default()) += 1;
+            } else if res == 'n'{
+                if guesscounts.contains_key(&c){
+                    guesscounts.get_mut(&c).unwrap().capped = true;
+                } else {
+                    guesscounts.insert(c, CharCount { capped: true, count: 0 });
+                }
             }
 
             if res == 'y' {
@@ -111,7 +134,7 @@ fn main() {
 
         // copy new guesscounts values to counts
         for (c, count) in guesscounts.iter() {
-            *counts.entry(*c).or_insert(0) = *count;
+            *counts.entry(*c).or_insert(Default::default()) = *count;
         }
 
         words.retain(|word| {
